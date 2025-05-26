e5726c8d076359201d9c55bc52034d6d204b8107
Aamosh12 <aamoshmaharjan234@gmail.com>
2025-05-26 13:12:23 +0545

    feat(todo): import Module, Custom Middleware and Controller in App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { TodoController } from './todo/todo.controller';
 
 @Module({
-  imports: [],
+  imports: [TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(TodoController);
+  }
+}
